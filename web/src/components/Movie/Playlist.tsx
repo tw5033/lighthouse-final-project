b14ff6d372cf0da5fdf7a780857da2f8e25affc5
tw5033 <tw5033@gmail.com>
2019-06-03 09:30:44 -0700

    Fix playlist button to toggle up and down

--- web/src/components/Movie/Playlist.tsx
+++ web/src/components/Movie/Playlist.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const Playlist = props => {
   const [isHidden, setIsHidden] = useState(false);
-  const [turnArrow, setTurnArrow] = useState("fas fa-3x fa-chevron-up");
+  const [turnArrow, setTurnArrow] = useState("fas fa-3x fa-chevron-up down");
   const playlist = props.playlist;
   // let firstPage;
   // let secondPage;
@@ -18,7 +18,7 @@ const Playlist = props => {
   // })
 
   const togglePlaylist = () => {
-    (turnArrow === "fas fa-3x fa-chevron-up down") ? setTurnArrow("fas fa-3x fa-chevron-up right") :
+    (turnArrow === "fas fa-3x fa-chevron-up down") ? setTurnArrow("fas fa-3x fa-chevron-up up") :
     setTurnArrow("fas fa-3x fa-chevron-up down");
     setIsHidden(isHidden => !isHidden);
   };
